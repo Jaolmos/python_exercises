cc01bf4b499dd8711bd0fab4aa8f53eec2fcbd6b
J.A Olmos <jose.a.olmosmartinez@gmail.com>
2022-01-29 14:08:14 +0100

    first upload minijuegos

---
+++ minijuegos/piedra_papel_tijera.py
@@ -0,0 +1,26 @@
+import random
+
+
+def play():
+    user = input("Escoge una opción: 'piedra', 'papel', 'tijera': ")
+    pc = random.choice(['piedra', 'papel', 'tijera'])
+
+    if user == pc:
+        return 'Empate!!'
+
+    if player_wins(user, pc):
+        return '¡¡¡Ganaste el juego!!'
+
+    return "¡¡¡Perdiste el juego!!!"
+
+
+def player_wins(player, opponent):
+    if ((player == 'piedra' and opponent == 'tijera)')
+        or (player == 'papel' and opponent == 'piedra')
+            or (player == 'tijera' and opponent == 'papel')):
+        return True
+    else:
+        return False
+
+
+print(play())
